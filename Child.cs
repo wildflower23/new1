1672739698fc2aaa24e93b08b8a12cd028c989df
wildflower23 <161136642+wildflower23@users.noreply.github.com>
2025-01-25 21:11:04 +0530

    Add files via upload

---
+++ Child.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public  class Child
+    {
+        public void sum(int x, int y)
+        {
+            Console.WriteLine(x + y);
+            //return x + y;
+        }
+        public void substract(int x, int y)
+        {
+            Console.WriteLine(x - y);
+          
+        }
+        public void multiply(int x, int y)
+        {
+            Console.WriteLine(x * y);
+            
+        }
+        public void division(int x, int y)
+        {
+            Console.WriteLine(x - y);
+           
+        }
+    }
+}
