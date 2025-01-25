52c08d25a8984d67367cacd20ac00cd422338279
wildflower23 <161136642+wildflower23@users.noreply.github.com>
2025-01-25 21:18:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+//List<Student> studentList = new List<Student>();
+//while (true)
+//{
+//    Console.Write("Enter Name:");
+//    string ename = Console.ReadLine();
+//    Console.Write("Enter Age:");
+//    int eage = Convert.ToInt32(Console.ReadLine());
+//    studentList.Add(new Student {name=ename,age=eage});
+
+//    Console.WriteLine("Name \t Age");
+//  foreach (var student in studentList)
+//    {
+//        Console.Write(student.name +"\t");
+//        Console.WriteLine(student.age);
+//    }
+//}
+//public class Student
+//{
+//    public string name { get; set; }
+//    public int age { get; set; }
+//}
+
+using ConsoleApp1;
+
+Child childobj = new Child();
+
+childobj.sum(5,6);
+childobj.substract(9, 6);
+childobj.multiply(5, 6);
+childobj.division(6, 3);
